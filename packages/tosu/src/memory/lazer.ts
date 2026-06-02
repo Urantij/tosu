32b0a6b64fa59b17f9bb21da70360a83dae98bc4
urantij <urantij@gmail.com>
2026-06-03 00:24:20 +0700

    change ranked play validation

--- packages/tosu/src/memory/lazer.ts
+++ packages/tosu/src/memory/lazer.ts
@@ -3763,14 +3763,18 @@ export class LazerMemory extends AbstractMemory<LazerPatternData> {
             return 'not-ready';
         }
 
-        // i believe its proved this state is rankedplay state
-
         const usersDictionary = this.process.readIntPtr(
             state +
                 this.offsets[
                     'osu.Game.Online.Multiplayer.MatchTypes.RankedPlay.RankedPlayRoomState'
                 ]['<Users>k__BackingField']
         );
+
+        if (!usersDictionary) {
+            return 'not-ready';
+        }
+
+        // i believe its proved this state is rankedplay state
 
         const users = readSharpDictionaryIntToRef(
             this.process,
