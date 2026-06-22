b1d873b9636067b7f38bf2508b3ca3eaa8fb10fc
urantij <urantij@gmail.com>
2026-06-22 11:28:03 +0700

    make userId higher than users

--- packages/tosu/src/memory/lazer.ts
+++ packages/tosu/src/memory/lazer.ts
@@ -3779,6 +3779,20 @@ export class LazerMemory extends AbstractMemory<LazerPatternData> {
             currentRound: round,
             starRating,
             damageMultiplier: damage,
+            activeUserId:
+                this.process.readNullableInt(
+                    state +
+                        this.offsets[
+                            'osu.Game.Online.Multiplayer.MatchTypes.RankedPlay.RankedPlayRoomState'
+                        ]['<ActiveUserId>k__BackingField']
+                ) || -1,
+            winningUserId:
+                this.process.readNullableInt(
+                    state +
+                        this.offsets[
+                            'osu.Game.Online.Multiplayer.MatchTypes.RankedPlay.RankedPlayRoomState'
+                        ]['<WinningUserId>k__BackingField']
+                ) || -1,
             users: users.map((u) => ({
                 id: u.key,
                 rating: this.process.readInt(
@@ -3805,19 +3819,7 @@ export class LazerMemory extends AbstractMemory<LazerPatternData> {
                             'osu.Game.Online.Multiplayer.MatchTypes.RankedPlay.RankedPlayUserInfo'
                         ]['<DamageMultiplier>k__BackingField']
                 )
-            })),
-            activeUserId: this.process.readNullableInt(
-                state +
-                    this.offsets[
-                        'osu.Game.Online.Multiplayer.MatchTypes.RankedPlay.RankedPlayRoomState'
-                    ]['<ActiveUserId>k__BackingField']
-            ),
-            winningUserId: this.process.readNullableInt(
-                state +
-                    this.offsets[
-                        'osu.Game.Online.Multiplayer.MatchTypes.RankedPlay.RankedPlayRoomState'
-                    ]['<WinningUserId>k__BackingField']
-            )
+            }))
         };
     }
 
